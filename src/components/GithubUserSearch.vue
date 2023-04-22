fa9131588166cdccfd933659ba79debfa73aad2e
Yoga2015 <yoga3333@126.com>
2023-04-22 23:14:59 +0800

    更改 点击搜索所触发的函数的名称为 searchUsers

--- src/components/GithubUserSearch.vue
+++ src/components/GithubUserSearch.vue
@@ -5,7 +5,7 @@
     <div>
       <input type="text" placeholder="输入你要搜索的名称" v-model="keyWord" />
       &nbsp;
-      <button @click="SearchAI">搜索</button>
+      <button @click="searchUsers">搜索</button>
     </div>
   </section>
 </template>
@@ -20,7 +20,7 @@ export default {
     }
   },
   methods: {
-    SearchAI() {
+    searchUsers() {
 
       // 请求前更新List的数据   (GithubUserList组件中的 动态数据 都是来自 GithubUserSearch组件)
       this.$bus.$emit('updataListData',
@@ -28,25 +28,25 @@ export default {
 
       axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
 
-         response => {
-           console.log('请求成功了');   // 此时 GithubUserSearch 已接收到响应回来的数据
+        response => {
+          console.log('请求成功了');   // 此时 GithubUserSearch 已接收到响应回来的数据
 
-           // 发送方  ，请求成功后，就把请求回来的数据 直接发给 GithubUserList组件
-           // this.$bus.$emit('getUsers', response.data.items)
+          // 发送方  ，请求成功后，就把请求回来的数据 直接发给 GithubUserList组件
+          // this.$bus.$emit('getUsers', response.data.items)
 
-           // 请求成功后更新List的数据
-           this.$bus.$emit('updataListData',
-             { isFirst: false, isLoading: false, errMsg: '', users: response.data.items }
-           )
-         },
+          // 请求成功后更新List的数据
+          this.$bus.$emit('updataListData',
+            { isFirst: false, isLoading: false, errMsg: '', users: response.data.items }
+          )
+        },
 
-         error => {
-           console.log('请求失败了', error.message);
-           // 请求失败后更新List的数据
-           this.$bus.$emit('updataListData',
-             { isFirst: false, isLoading: false, errMsg: error.message, users: [] }
-           )
-         }
+        error => {
+          console.log('请求失败了', error.message);
+          // 请求失败后更新List的数据
+          this.$bus.$emit('updataListData',
+            { isFirst: false, isLoading: false, errMsg: error.message, users: [] }
+          )
+        }
 
       )
     }
